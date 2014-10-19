c0d92b0966a9a20c20974b70fede8d408e9a0500
jangemue <jan.muehlig@udo.edu>
2014-10-20 00:37:10 +0200

    Removed checkbox-filter (no usage for such kind of filters), added structure for date filter.

---
+++ Table/Filter/DateFilter.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace PZAD\TableBundle\Table\Filter;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Filter for date columns.
+ * 
+ * @author	Jan Mühlig
+ * @since	1.0
+ */
+class DateFilter extends AbstractFilter
+{
+	public function needsFormEnviroment()
+	{
+		return true;
+	}
+	
+	protected function setDefaultFilterOptions(OptionsResolver $optionsResolver)
+	{
+		parent::setDefaultFilterOptions($optionsResolver);
+		
+		$optionsResolver->setDefaults(array(
+			'format' => 'd.m.Y',
+			'widget' => 'choice',
+			'years' => range( date('Y', strtotime('-5 years')), date('Y', strtotime('+5 years')) ),
+			'days' => range(1,31)
+		));
+		
+		$optionsResolver->setAllowedValues(array(
+			'widget' => array('choice', 'text', 'single_text')
+		));
+	}
+
+	public function render(ContainerInterface $container)
+	{
+		if($this->widget === 'single_text')
+		{
+			return $this->renderSingleText($container);
+		}
+		else if($this->widget === 'text')
+		{
+			return $this->renderText($container);
+		}
+		else
+		{
+			return $this->renderChoice($container);
+		}
+	}
+	
+	protected function renderChoice(ContainerInterface $container)
+	{
+		$content = "";
+		
+		// 
+		
+		return $content;
+	}
+	
+	protected function renderText(ContainerInterface $container)
+	{
+		
+	}
+	
+	protected function renderSingleText(ContainerInterface $container)
+	{
+		
+	}
+}
