3ca817ddeb85201d31eed30ee81ff4d3df8fc0fa
jangemue <jan.muehlig@udo.edu>
2015-12-30 14:28:10 +0100

    Renamed AnonymouseTableBuilder -> TableTypeBuilder, moved TableFactory to services.

--- Table/TableTypeBuilder.php
+++ Table/TableTypeBuilder.php
@@ -7,18 +7,22 @@ use JGM\TableBundle\Table\DataSource\ArrayDataSource;
 use JGM\TableBundle\Table\DataSource\DataSourceInterface;
 use JGM\TableBundle\Table\DataSource\EntityDataSource;
 use JGM\TableBundle\Table\DataSource\QueryBuilderDataSource;
+use JGM\TableBundle\Table\Filter\FilterBuilder;
 use JGM\TableBundle\Table\Filter\Type\FilterTypeInterface;
 use JGM\TableBundle\Table\Order\Type\OrderTypeInterface;
 use JGM\TableBundle\Table\Pagination\Type\PaginationTypeInterface;
 use JGM\TableBundle\Table\Type\AbstractTableType;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
- * Description of AnonymousTableBuilder
- *
- * @author Jan
+ * TableTypeBuilder for building tables without any table type, 
+ * i.e. at the controller.
+ * 
+ * @author	Jan Mühlig <mail@janmuehlig.de>
+ * @since	1.2
  */
-class AnonymousTableBuilder extends AbstractTableType implements PaginationTypeInterface, OrderTypeInterface, FilterTypeInterface
+class TableTypeBuilder extends AbstractTableType implements PaginationTypeInterface, OrderTypeInterface, FilterTypeInterface
 {
 	/**
 	 * @var string 
@@ -78,6 +82,15 @@ class AnonymousTableBuilder extends AbstractTableType implements PaginationTypeI
 		$this->paginationOptions = array();
 	}
 	
+	/**
+	 * Adds a column to the table.
+	 * 
+	 * @param string $type			Type of the column.
+	 * @param string $name			Name of the column.
+	 * @param array $options		Options of the column.
+	 * 
+	 * @return TableTypeBuilder
+	 */
 	public function addColumn($type, $name, array $options = array())
 	{
 		$this->columns[] = array($type, $name, $options);
@@ -85,6 +98,15 @@ class AnonymousTableBuilder extends AbstractTableType implements PaginationTypeI
 		return $this;
 	}
 	
+	/**
+	 * Adds an entity data source to the table for the given entity.
+	 * 
+	 * @param string $entity		Name of the Entity, i.e. 'AcmeBundle:Car'.
+	 * @param string $alias			Alias for the query builder.
+	 * @param callback $callback	Callback, which takes a ORM QueryBuilder as parameter, for manipulating the query.
+	 * 
+	 * @return TableTypeBuilder
+	 */
 	public function forEntity($entity, $alias = 't', $callback = null)
 	{
 		$this->setDataSource(new EntityDataSource($entity, $alias, $callback));
@@ -92,6 +114,13 @@ class AnonymousTableBuilder extends AbstractTableType implements PaginationTypeI
 		return $this;
 	}
 	
+	/**
+	 * Adds a query builder data source to the table for the given query builder.
+	 * 
+	 * @param QueryBuilder $queryBuilder	QueryBuilder.
+	 * 
+	 * @return TableTypeBuilder
+	 */
 	public function forQuery(QueryBuilder $queryBuilder)
 	{
 		$this->setDataSource(new QueryBuilderDataSource($queryBuilder));
@@ -99,6 +128,13 @@ class AnonymousTableBuilder extends AbstractTableType implements PaginationTypeI
 		return $this;
 	}
 	
+	/**
+	 * Adds an array data source to the table for the given array.
+	 * 
+	 * @param array $data	Data, that will be displayed at the table.
+	 * 
+	 * @return TableTypeBuilder
+	 */
 	public function forArray(array $data)
 	{
 		$this->setDataSource(new ArrayDataSource($data));
@@ -106,6 +142,13 @@ class AnonymousTableBuilder extends AbstractTableType implements PaginationTypeI
 		return $this;
 	}
 	
+	/**
+	 * Sets a given data source.
+	 * 
+	 * @param DataSourceInterface $dataSource	DataSource, which will organize the data of the table.
+	 * 
+	 * @return TableTypeBuilder
+	 */
 	public function setDataSource(DataSourceInterface $dataSource)
 	{
 		$this->dataSource = $dataSource;
@@ -113,6 +156,13 @@ class AnonymousTableBuilder extends AbstractTableType implements PaginationTypeI
 		return $this;
 	}
 	
+	/**
+	 * Enables pagination with the given options.
+	 * 
+	 * @param array $options	Options for pagination.
+	 * 
+	 * @return TableTypeBuilder
+	 */
 	public function enablePagination(array $options = array())
 	{
 		$this->tableOptions['use_pagination'] = true;
@@ -121,6 +171,13 @@ class AnonymousTableBuilder extends AbstractTableType implements PaginationTypeI
 		return $this;
 	}
 	
+	/**
+	 * Enables order with the given options.
+	 * 
+	 * @param array $options	Options for order.
+	 * 
+	 * @return TableTypeBuilder
+	 */
 	public function enableOrder(array $options = array())
 	{
 		$this->tableOptions['use_order'] = true;
@@ -129,6 +186,13 @@ class AnonymousTableBuilder extends AbstractTableType implements PaginationTypeI
 		return $this;
 	}
 	
+	/**
+	 * Enables filters with the given options.
+	 * 
+	 * @param array $options	Options for filters.
+	 * 
+	 * @return TableTypeBuilder
+	 */
 	public function enableFilter(array $options = array())
 	{
 		$this->tableOptions['use_filter'] = true;
@@ -137,6 +201,15 @@ class AnonymousTableBuilder extends AbstractTableType implements PaginationTypeI
 		return $this;
 	}
 	
+	/**
+	 * Adds a filter to the table.
+	 * 
+	 * @param string $type			Type of the filter.
+	 * @param string $name			Name of the filter.
+	 * @param array $options		Options of the filter.
+	 * 
+	 * @return TableTypeBuilder
+	 */
 	public function addFilter($type, $name, array $options = array())
 	{
 		$this->filters[] = array($type, $name, $options);
@@ -144,6 +217,11 @@ class AnonymousTableBuilder extends AbstractTableType implements PaginationTypeI
 		return $this;
 	}
 	
+	/**
+	 * Returns the table.
+	 * 
+	 * @return Table
+	 */
 	public function getTable()
 	{
 		return $this->table->create($this, $this->options);
@@ -167,7 +245,7 @@ class AnonymousTableBuilder extends AbstractTableType implements PaginationTypeI
 		}
 	}
 
-	public function buildFilter(Filter\FilterBuilder $filterBuilder)
+	public function buildFilter(FilterBuilder $filterBuilder)
 	{
 		foreach($this->filters as $filter)
 		{
@@ -175,17 +253,17 @@ class AnonymousTableBuilder extends AbstractTableType implements PaginationTypeI
 		}
 	}
 
-	public function configureFilterButtonOptions(\Symfony\Component\OptionsResolver\OptionsResolver $resolver)
+	public function configureFilterButtonOptions(OptionsResolver $resolver)
 	{
 		$resolver->setDefaults($this->filterOptions);
 	}
 
-	public function configureOrderOptions(\Symfony\Component\OptionsResolver\OptionsResolver $resolver)
+	public function configureOrderOptions(OptionsResolver $resolver)
 	{
 		$resolver->setDefaults($this->orderOptions);
 	}
 
-	public function configurePaginationOptions(\Symfony\Component\OptionsResolver\OptionsResolver $resolver)
+	public function configurePaginationOptions(OptionsResolver $resolver)
 	{
 		$resolver->setDefaults($this->paginationOptions);
 	}
