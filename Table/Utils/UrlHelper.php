500875035f6628e45d2cfb5be7f18257a0d38e6b
Daniel <daniel.purrucker@nordakademie.de>
2016-05-04 11:27:08 +0200

    If there is no request, return empty string as url

--- Table/Utils/UrlHelper.php
+++ Table/Utils/UrlHelper.php
@@ -50,6 +50,9 @@ class UrlHelper
 	 */
 	public function getUrlForParameters(array $replacedParameters = array(), $anchor = null)
 	{
+		if(null === $this->request) {
+			return '';
+		}
 		$routeName = $this->request->get('_route');
 		$currentRouteParams = array_merge(
 			$this->request->attributes->get('_route_params'),
